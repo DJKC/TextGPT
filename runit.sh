b0edee77163d1e0e00b6cf0d294536a8a9fd659f
DJKC <lasjapas@yahoo.com>
2023-03-26 22:51:22 -0700

    Trying to change the access to ssh from HTTPS.

---
+++ runit.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Clone the private Git repository using an access token
+ACCESS_TOKEN="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"
+
+REPO_URL="https://github.com/DJKC/TextGPT.git"
+LOCAL_DIR="/home/ec2-user/Desktop/TextGPT"
+FILE_TO_PULL="OpenAI.py"
+
+# Clone the Git repository if it doesn't exist
+if [ ! -d "$LOCAL_DIR" ]; then
+    git clone $REPO_URL --branch master --single-branch "$LOCAL_DIR" -q
+    if [ $? -ne 0 ]; then
+        echo "Error: Git clone failed. Exiting..."
+        exit 1
+    fi
+fi
+
+# Pull and checkout the latest version of the specified file
+cd "$LOCAL_DIR"
+git pull origin master
+git checkout origin/master -- "$FILE_TO_PULL"
+if [ $? -ne 0 ]; then
+    echo "Error: Git checkout failed. Exiting..."
+    exit 1
+fi
+
+# Check if the specified file exists
+if [ ! -f "$LOCAL_DIR/$FILE_TO_PULL" ]; then
+    echo "Error: The specified file ($FILE_TO_PULL) was not found in $LOCAL_DIR. Exiting..."
+    exit 1
+fi
+
+# Run the Python script
+python3 "$LOCAL_DIR/OpenAI.py"
